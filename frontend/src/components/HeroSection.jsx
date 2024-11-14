31e5f03e69012f78e17017cabfde655a8c2fa97d
Sachin Gavali <sachingavali321@gmail.com>
2024-11-14 23:24:02 +0530

    hero section chanegd

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -5,10 +5,11 @@ const HeroSection = () => {
     <main className="w-full bg-slate-900 min-h-screen">
       <div className="container  text-white mx-auto grid grid-cols-2 gap-4">
         <img src="/uploads/hero-img.jpg" alt="" />
-        <div>
+        <div className="">
           <img src="/uploads/1731409766453.jpg" alt="" />
           <img src="/uploads/1731410160643.jpg" alt="" />
         </div>
+        <img src="/uploads/1731410160643.jpg" alt="" />
       </div>
     </main>
   );
